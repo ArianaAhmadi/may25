7d35f4116d65d88f7a1670d0cf8ce23b168249d7
ArianaAhmadi1 <>
2021-05-26 01:08:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#a=input()
+#print(' '.join([str(i) for i in a]))
+#a is input() 
+#Then print the value(' '.join the([str(i) for the i in a]))
+
+a=input() 
+b=a.replace(max(a),' ')
+c=max(b)
+print(c)
+#a is intput()
+#b is a replacing (max of a, ' ')
+#c is max of b
+#Then print c
